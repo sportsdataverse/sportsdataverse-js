16ab0e0b45b254d71d5e3df224726d3609d5a638
Saiem Gilani <saiem.gilani@gmail.com>
2021-02-23 23:40:31 -0500

    same date configuring

--- app/ncaa/services/scoreboard.service.js
+++ app/ncaa/services/scoreboard.service.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 //  'beach-volleyball', 'lacrosse-men','lacrosse-women', 'volleyball-men']
 // must use parameter - division = 'fbs' for football
 exports.getNcaaScoreboard = async (sport,division,year,month ,day) => {
-    const baseUrl = `https://data.ncaa.com/casablanca/scoreboard/${sport}/${division}/${year}/${month}/${day}/scoreboard.json`;
+    const baseUrl = `https://data.ncaa.com/casablanca/scoreboard/${sport}/${division}/${year}/${parseInt(month) <= 9 ? "0" + month : month}/${parseInt(day) <= 9 ? "0" + day : day}/scoreboard.json`;
 
     const res = await axios.get(baseUrl);
     return res.data;
